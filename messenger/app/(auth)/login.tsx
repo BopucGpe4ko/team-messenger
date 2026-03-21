53c9509a11a4604a5313b59e1c744b19a4637468
BopucGpe4ko <bopucgpe4ko@gmail.com>
2026-03-21 18:50:00 +0100

    Added authorization with Clerk
    Added authorization with Clerk and login page

---
+++ messenger/app/(auth)/login.tsx
@@ -0,0 +1,66 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "@/assets/styles/auth.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+import { useRouter } from "expo-router";
+import { useSSO } from "@clerk/expo";
+
+export default function LoginScreen() {
+  const { startSSOFlow } = useSSO();
+  const router = useRouter();
+
+  const handleGooglePress = async () => {
+    console.log("Google pressed");
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: "oauth_google",
+      });
+
+      if (createdSessionId && setActive) {
+        await setActive({ session: createdSessionId });
+        router.replace("/(tabs)");
+      }
+    } catch (error) {
+      console.error("Google login failed", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.brandSection}>
+        <View style={styles.logoContainer}>
+          <Ionicons
+            name="accessibility-outline"
+            size={24}
+            color={COLORS.primary}
+          />
+        </View>
+        <Text style={styles.appName}>UA-Messenger</Text>
+        <Text style={styles.tagline}>Find your next adventure</Text>
+      </View>
+
+      <View style={styles.illustrationContainer}>
+        <Image
+          resizeMode="contain"
+          source={require("@/assets/images/android-icon-foreground.png")}
+          style={styles.illustration}
+        />
+      </View>
+
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          activeOpacity={0.9}
+          onPress={() => {
+            handleGooglePress();
+          }}
+        >
+          <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={24} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Continue with Google</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
--- messenger/app/_layout.tsx
+++ messenger/app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import InitialLayout from "@/components/initial.layout";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+        <InitialLayout />
+      </SafeAreaView>
+    </ClerkProvider>
+  );
 }
