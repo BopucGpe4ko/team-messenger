b5a069bd64a48c61ecabd02279fb1ea519862abc
BopucGpe4ko <bopucgpe4ko@gmail.com>
2026-03-25 14:42:46 +0100

    Create ClerkAndConvexProvider
    Moved Clerk and Convex providers to a separate file

--- messenger/app/_layout.tsx
+++ messenger/app/_layout.tsx
@@ -1,25 +1,39 @@
-import InitialLayout from "@/components/initial.layout";
-import { ClerkProvider, useAuth } from "@clerk/expo";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
-import { tokenCache } from "@clerk/expo/token-cache";
+// import InitialLayout from "@/components/initial.layout";
+// import { ClerkProvider, useAuth } from "@clerk/expo";
+// import { ConvexProviderWithClerk } from "convex/react-clerk";
+// import { ConvexReactClient } from "convex/react";
+// import { tokenCache } from "@clerk/expo/token-cache";
+// import { SafeAreaView } from "react-native-safe-area-context";
+
+// const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+// const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
+
+// if (!publishableKey) {
+//   throw new Error("Add your Clerk Publishable Key to the .env file");
+// }
+
+// export default function RootLayout() {
+//   return (
+//     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+//       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+//         <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+//           <InitialLayout />
+//         </SafeAreaView>
+//       </ConvexProviderWithClerk>
+//     </ClerkProvider>
+//   );
+// }
+
 import { SafeAreaView } from "react-native-safe-area-context";
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
-
-if (!publishableKey) {
-  throw new Error("Add your Clerk Publishable Key to the .env file");
-}
+import InitialLayout from "@/components/initial.layout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-          <InitialLayout />
-        </SafeAreaView>
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+    <ClerkAndConvexProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#000000" }}>
+        <InitialLayout />
+      </SafeAreaView>
+    </ClerkAndConvexProvider>
   );
 }
